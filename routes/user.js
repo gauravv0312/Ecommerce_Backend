5397fea9f7e6eb5a12aef9e832d6f8f4295549ae
gauravv0312 <gauravv0312@gmail.com>
2023-01-17 16:38:04 +0530

    logout feature is done

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { get } = require('mongoose');
 const router = express.Router();
-const {signup,login} = require("../controller/userController");
+const {signup,login,logout} = require("../controller/userController");
 const user = require('../model/user');
 router.route('/signup').post(signup);
 router.route('/login').post(login);
+router.route('/logout').get(logout);
 
 module.exports = router ;
